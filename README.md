c1c131cdd767727b46c36c4e5b6be61fc1669dc1
Dien Tran <usbv135w@gmail.com>
2020-05-24 07:26:57 -0500

    Dark theme, syntax highlighting enabled

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mds
 
-A fork of [tlight's md](https://github.com/tlight/md) that uses the [goldmark](https://github.com/yuin/goldmark) renderer instead of [blackfriday](https://github.com/russross/blackfriday/). As is the original project:
+A fork of [tlight's md](https://github.com/tlight/md) that uses the [Goldmark](https://github.com/yuin/goldmark) renderer instead of [Blackfriday](https://github.com/russross/blackfriday/). As is the original project:
 
 > Zero configuration minimal markdown server for local rendering.
 
@@ -13,13 +13,13 @@ go get github.com/dienakakim/mds
 ## Usage
 
 ```bash
-mds README.md
+mds --file=README.md
 ```
 
 ## License
 
-Copyright (c) 2018 Tim, (c) 2020 Dien Tran. See LICENSE file.
+Copyright (c) 2020 Dien Tran. See LICENSE file. Enough parts of the code have been rewritten that I can safely pronounce it "original".
 
 ## P.S
 
-`md` is a bad name for a project, as it clashes with the "make directory" command on Windows. This spurred me to initiate this fork, but that is not the only fix this fork makes: upcoming features include syntax highlighting and a dark themed render.
+`md` is a bad name for a project, as it clashes with the "make directory" command on Windows. This spurred me to initiate this fork, but that is not the only fix this fork makes: a dark theme (enabled with the flag `--dark`) and syntax highlighting for your favorite language are now included.
--- mds.go
+++ mds.go
@@ -31,7 +31,8 @@ Usage: ${prog} --file=FILE.md
 var errorText = "Failed to parse markdown"
 
 type RenderedHTML struct {
-	Body template.HTML
+	Body  template.HTML
+	Style template.CSS
 }
 
 // main is the driver code for the program.
@@ -49,29 +50,36 @@ func main() {
 	}
 
 	// Create template
-	var htmlTemplateBytes []byte
+	var htmlTemplateBytes, styleBytes []byte
 	if *dark {
-		log.Fatal("Not implemented!")
-	} else {
-		htmlTemplateB, err := Asset("template.html")
+		styleBytes_, err := assetsDarkOutCssBytes()
 		if err != nil {
 			log.Fatal(err)
 		}
-		htmlTemplateBytes = htmlTemplateB
+		styleBytes = styleBytes_
+	} else {
+		styleBytes_, err := assetsLightOutCssBytes()
+		if err != nil {
+			log.Fatal(err)
+		}
+		styleBytes = styleBytes_
 	}
-	htmlTemplate := string(htmlTemplateBytes)
-	templ, err := template.New("md").Parse(htmlTemplate)
+	htmlTemplateBytes, err := assetsIndexHtmlBytes()
+	if err != nil {
+		log.Fatal(err)
+	}
+	templ, err := template.New("md").Parse(string(htmlTemplateBytes))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Create a goldmark instance with GFM extensions
+	// Create a Goldmark instance with GFM extensions
 	gm := goldmark.New(goldmark.WithExtensions(extension.GFM, highlighting.NewHighlighting(highlighting.WithStyle("monokailight"))))
 
 	// Create new ServeMux
 	sm := http.NewServeMux()
 	sm.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		render(w, r, gm, *file, templ)
+		render(w, r, gm, *file, templ, string(styleBytes))
 	})
 
 	// Serve
@@ -91,8 +99,8 @@ func usage(note string) {
 	os.Exit(0)
 }
 
-// render uses the given goldmark instance to render the HTML.
-func render(w http.ResponseWriter, r *http.Request, gm goldmark.Markdown, filename string, templ *template.Template) {
+// render uses the given Goldmark instance to render the HTML.
+func render(w http.ResponseWriter, r *http.Request, gm goldmark.Markdown, filename string, templ *template.Template, css string) {
 	markdown, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -101,6 +109,6 @@ func render(w http.ResponseWriter, r *http.Request, gm goldmark.Markdown, filena
 	if err := gm.Convert(markdown, &html); err != nil {
 		log.Println(errorText)
 	}
-	rendered := RenderedHTML{Body: template.HTML(html.String())}
+	rendered := RenderedHTML{Body: template.HTML(html.String()), Style: template.CSS(css)}
 	templ.Execute(w, rendered)
 }
